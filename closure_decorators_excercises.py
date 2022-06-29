9be1c82edf05c60dfc1d1aa17b52b49d0191d2c9
Yiting Xiang <xiangyiting@gmail.com>
2022-06-29 00:17:12 -0700

    I finished the assisgnment

--- closure_decorators_excercises.py
+++ closure_decorators_excercises.py
@@ -3,10 +3,18 @@
 #Closure Excercise
 #Using a closure, create a function, multiples_of(n) which we can use to
 #create generators that generate multiples of n less than a given number.
+def multiples_of(n):
+    def multiples(m):
+      for item in range(1,m):
+          if item%n ==0:
+            yield item
+            
+    return multiples
 
 m3 = multiples_of(3)
 m3_under30 = m3(30)
 m7_under30 = multiples_of(7)(30)
+
 
 print(type(m3_under30))
 # output: <class 'generator'>
@@ -21,9 +29,15 @@ print(*m7_under30)
 #Decorators Excercise 1
 #@make_upper – make every letter of a string returned from the decorated
 #function uppercase.
+def mark_upper(func):
+  def wapper():
+    return func().upper()
+  return wapper
 
 def hello_world():
     return 'hello young, good day!!'
+
+hello_world=mark_upper(hello_world)
 
 print(hello_world()) # output: HELLO YOUNG, GOOD DAY!!
 #-----------------------------------------------------------------------
@@ -31,10 +45,16 @@ print(hello_world()) # output: HELLO YOUNG, GOOD DAY!!
 #Decorators Excercise 2
 #@print_func_name – print the name of the decorated function before
 #executing the function.
+def print_func_name(func): 
+  def wapper():
+    print(func.__name__, "is running...")
+    return func()
+  return wapper
 
 def my_func():
     print('Python is fun!!')
 
+my_func= print_func_name(my_func)
 my_func() # output: my_func is running...
             #Python is fun
 #----------------------------------------------------------------------
@@ -42,19 +62,32 @@ my_func() # output: my_func is running...
 #Decoratos Excercise 3
 #@give_name(name) – concatenate the given name at the end of a string
 #returned from the decorated function.
+def give_name(name):
+  def dec(func):
+    def wrapper():
+      return func() + ' ' + name
+    return wrapper
+  return dec
+
 def greeting():
     return 'Hello'
-
+greeting=give_name('Theresa')(greeting)
 print(greeting()) # output: Hello Theresa
 #---------------------------------------------------------------------
 
 #Decorators Excercise 4
 #@print_input_type – print a data type of the input argument before
 #executing the decorated function.
+def print_input_type(func):
+  def wapper(num):
+    print('The input data type is ', type(num))
+    return func(num)
+  return wapper
 
 def square(n):
     return n ** 2
 
+square=print_input_type(square)
 print(square(3.5)) # output: The input data type is <class 'float'>
                     #12.25
 #-------------------------------------------------------------------
@@ -63,11 +96,21 @@ print(square(3.5)) # output: The input data type is <class 'float'>
 #@check_return_type(return_type) – check if the return type of the
 #decorated function is return_type and print the result before executing
 #the function.
+def check_return_type(func,test_type):
+  def return_type(item):
+    result=func(item)
+    if not isinstance(result, test_type):
+      print('==========Error!!')
+      print('The return type is NOT', test_type)
+    else:
+      print('The return type is ',type(item))
+    return result
+  return return_type
 
 #pass in a string
 def square(n):
     return n ** 2
-
+square=check_return_type(square, str)
 print(square(6)) # output: =========Error!!
                     #The return type is NOT <class 'str'>
                     #36
@@ -75,13 +118,19 @@ print(square(6)) # output: =========Error!!
 #pass in a float
 def square(n):
     return n ** 2
-
+square=check_return_type(square, float)
 print(square(2.9)) # output: The return type is <class 'float'>
                     #8.41
 #------------------------------------------------------------------------
 
 #Decorators Excercise 6
 #@execute_log – write a function execution log on the log file. (log below)
+import datetime
+def execute_log(func):
+  def wapper(*args):
+    print(datetime.datetime.now(), func.__name__)
+    return func(*args)
+  return wapper
 
 def multiply(*nums):
     mult = 1
@@ -92,6 +141,8 @@ def multiply(*nums):
 def hello_world():
     return 'hello world!!'
 
+multiply=execute_log(multiply)
+hello_world=execute_log(hello_world)
 print(multiply(6, 2, 3)) # 36
 print(hello_world()) # hello world!!
 print(multiply(2.2, 4)) # 8.8
